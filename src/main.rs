14bb4b080145ae063294c32438a2d31b40744515
tonymushah <tonyramanantsalama@gmail.com>
2024-03-11 12:37:59 +0300

    fixing lint errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::thread;
+use std::{thread, time::Duration};
 
-fn sleepsort<I: Iterator<Item=u32>>(nums: I) {
-    let threads: Vec<_> = nums.map(|n|
-        thread::spawn(move || {
-            thread::sleep_ms(n);
-            println!("{}", n); })).collect();
-    for t in threads { t.join(); }
+fn sleepsort<I: Iterator<Item = u32>>(nums: I) {
+    let threads: Vec<_> = nums
+        .map(|n| {
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(n.into()));
+                println!("{}", n);
+            })
+        })
+        .collect();
+    for t in threads {
+        let _ = t.join();
+    }
 }
 
 fn main() {
